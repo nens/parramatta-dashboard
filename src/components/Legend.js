8ca47332ce2e93a770caf3f32535f53a8447f965
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-10-11 10:41:16 +0200

    (fix): Adds legend animation

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -9,7 +9,8 @@ class Legend extends Component {
 
     this.state = {
       width: window.innerWidth,
-      height: window.innerHeight
+      height: window.innerHeight,
+      isOpen: false
     };
     this.handleUpdateDimensions = this.handleUpdateDimensions.bind(this);
   }
@@ -29,11 +30,22 @@ class Legend extends Component {
     });
   }
   render() {
-    const { width } = this.state;
+    const { width, isOpen } = this.state;
     const isMobile = width < 700 ? true : false;
     return (
-      <div className={isMobile ? styles.LegendMobile : styles.Legend}>
-        <div className={styles.LegendTitle}>Legenda</div>
+      <div className={isMobile ? styles.LegendMobile : styles.Legend} style={{
+        bottom: (isOpen) ? 0 : -390
+      }}>
+        <div
+          className={styles.LegendTitle}
+          onClick={() =>
+            this.setState({
+              isOpen: !isOpen
+            })}
+        >
+          Legend
+          <i className="material-icons">drag_handle</i>
+        </div>
       </div>
     );
   }
--- src/components/TimeseriesTile.js
+++ src/components/TimeseriesTile.js
@@ -286,7 +286,7 @@ class TimeseriesChartComponent extends Component {
 
   render() {
     const { tile } = this.props;
-    console.log(this.props);
+
     if (!this.allTimeseriesHaveMetadata() || !this.allTimeseriesHaveEvents()) {
       return null;
     }
