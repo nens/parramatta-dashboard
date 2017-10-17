92efa57e2d3ee9b606be4c597cbbbbdf4a4fc212
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-10-17 21:03:51 +0200

    Styling statistics tile (small)

--- src/components/StatisticsTile.js
+++ src/components/StatisticsTile.js
@@ -2,13 +2,31 @@ import React, { Component } from "react";
 import styles from "./StatisticsTile.css";
 
 class StatisticsTile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: null,
+      height: null
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      width: this.myInput.offsetWidth,
+      height: this.myInput.offsetHeight
+    })
+  }
   render() {
     const { title, number } = this.props;
+    const { width } = this.state;
     return (
       <div
+        ref={input => {this.myInput = input}}
         className={styles.StatisticsTile}
       >
-        <div><p>{number}</p><span>{title}</span></div>
+        <div>
+          <p>{number}</p>
+          {width > 200 ? <span>{title}</span> : null}
+        </div>
       </div>
     );
   }
