7296d1918c20265445fc025c5015471b9d9cb8d3
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-10-17 13:39:51 +0200

    fix(Assetmap): Asset tile works

--- src/components/MapTile.js
+++ src/components/MapTile.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
+import { BOUNDS } from "../config";
 import { connect } from "react-redux";
-import { getRaster } from "../actions";
-import { Map, TileLayer, WMSTileLayer } from "react-leaflet";
+import { getRaster, addAsset } from "../actions";
+import { getMeasuringStations } from "lizard-api-client";
+import { Map, TileLayer, Marker, WMSTileLayer } from "react-leaflet";
 import styles from "./MapTile.css";
 
 class MapTile extends Component {
+  componentDidMount() {
+    const { tile } = this.props;
+    const inBboxFilter = this.getBbox().toLizardBbox();
+
+    if (tile.assetTypes) {
+      this.props.tile.assetTypes.forEach(assetType => {
+        // This is really impossible, need something more generic
+        if (assetType === "measuringstation") {
+          getMeasuringStations({
+            in_bbox: inBboxFilter,
+            page_size: 1000
+          }).then(results => {
+            results.forEach(measuringStation => {
+              this.props.addAsset(
+                "measuringstation",
+                measuringStation.id,
+                measuringStation
+              );
+            });
+          });
+        }
+      });
+    }
+  }
+  getBbox() {
+    // Either get it from the tile or return the global constant.
+    if (this.props && this.props.tile && this.props.tile.bbox) {
+      return this.props.tile.bbox;
+    }
+    return BOUNDS;
+  }
   tileLayerForRaster(raster) {
     let rasterObject = null;
     if (this.props.rasters.hasOwnProperty(raster.uuid)) {
@@ -46,6 +79,11 @@ class MapTile extends Component {
           [-32.76800155639643, 152.0842590332031]
         ];
 
+    const assets = tile.assetTypes ? this.props.assets[tile.assetTypes] : {};
+    const markers = Object.values(assets).map((asset, index) => {
+      const { coordinates } = asset.geometry;
+      return <Marker position={[coordinates[1],coordinates[0]]} key={asset.id} />;
+    });
 
     return (
       <div className={styles.MapTile}>
@@ -65,23 +103,15 @@ class MapTile extends Component {
         >
           <TileLayer url="https://{s}.tiles.mapbox.com/v3/nelenschuurmans.iaa98k8k/{z}/{x}/{y}.png" />
           {/* <Marker position={[-33.815, 151.0087]} /> */}
-          {tile.rasters ? tile.rasters.map(raster =>
-            this.tileLayerForRaster(raster)
-          ) : null}
-
-          {/*
-          // TODO: Assets?!?!?
-          {tile.assetTypes ? tile.assetTypes.map(asset =>
-          ) : null}
-          */}
+          {tile.rasters
+            ? tile.rasters.map(raster => this.tileLayerForRaster(raster))
+            : null}
+          {markers}
         </Map>
       </div>
     );
   }
 }
-
-
-
 
 function mapStateToProps(state) {
   return {
@@ -92,7 +122,9 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    getRaster: uuid => dispatch(getRaster(uuid)),
+    addAsset: (assetType, id, instance) =>
+      dispatch(addAsset(assetType, id, instance)),
+    getRaster: uuid => dispatch(getRaster(uuid))
   };
 }
 
