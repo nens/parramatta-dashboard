7e7dfe72509f4b3a4ee89db1efcaec4f0da368a3
Gijs Nijholt <gijs.nijholt@gmail.com>
2017-11-01 16:06:53 +0100

    Full mode Alarms (#45)
    * fix(FullStatistics): Styling and table view for alarms in full mode.
    
    * rm console.log

--- src/components/FullStatistics.js
+++ src/components/FullStatistics.js
@@ -1,19 +1,92 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchAlarms } from "../actions";
 import styles from "./FullStatistics.css";
 
 class FullStatistics extends Component {
+  componentDidMount() {
+    this.props.fetchAlarms();
+  }
+  getAlarmsTable() {
+    const { alarms } = this.props;
+    const alarmsSortedByName = alarms.data
+      ? alarms.data.slice().sort((a, b) => {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+          return 0;
+        })
+      : [];
+    const alarmRows = alarmsSortedByName.map((alarm, i) => {
+      return (
+        <tr key={i}>
+          <td>
+            {alarm.active ? (
+              <i className="material-icons" style={{ color: "red" }}>
+                warning
+              </i>
+            ) : (
+              <i className="material-icons" style={{ color: "green" }}>
+                check_circle
+              </i>
+            )}
+          </td>
+          <td>{alarm.name}</td>
+          <td>
+            {alarm.warning_threshold
+              ? alarm.warning_threshold.warning_level
+              : null}
+          </td>
+          <td>
+            {alarm.warning_value
+              ? alarm.warning_value
+              : null}
+          </td>
+        </tr>
+      );
+    });
+    return (
+      <table className={styles.Table}>
+        <thead>
+          <tr>
+            <td>&nbsp;</td>
+            <td>Alarm name</td>
+            <td>Level</td>
+            <td>Value</td>
+          </tr>
+        </thead>
+        <tbody>{alarmRows}</tbody>
+      </table>
+    );
+  }
   render() {
-    const { tile, height, isMobile } = this.props;
+    const { tile, height, isMobile, alarms } = this.props;
+    const numberOfAlarms = (alarms.data) ? alarms.data.length : 0;
     return (
       <div
         className={styles.FullStatistics}
         style={{ height, paddingLeft: isMobile ? 0 : 200 }}
       >
-        <div className={styles.Number}>{tile.number}</div>
-        <div className={styles.Title}>{tile.title}</div>
+        <div className={styles.Title}>{tile.title} ({numberOfAlarms})</div>
+        {this.getAlarmsTable()}
       </div>
     );
   }
 }
 
-export default FullStatistics;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    alarms: state.alarms
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    fetchAlarms: alarmsState => fetchAlarms(dispatch, alarmsState)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(FullStatistics);
