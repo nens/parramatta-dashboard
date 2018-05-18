3fa151955e40e0170fb446eedb9807503171ba1c
Remco Gerlich <remco.gerlich@nelen-schuurmans.nl>
2018-05-18 10:52:42 +0200

    Use /flash instead of /fish as URL

--- src/TermsOrLoginOrApp.js
+++ src/TermsOrLoginOrApp.js
@@ -47,7 +47,7 @@ class TermsOrLoginOrAppComponent extends Component {
       return <TermsAndConditions termsSigned={this.termsSigned.bind(this)} />;
     } else {
       return (
-        <Router basename="/fish">
+        <Router basename="/flash">
           <App />
         </Router>
       );
