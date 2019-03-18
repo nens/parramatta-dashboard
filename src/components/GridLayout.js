af3693e5920b913c4886694dbc5806093116e0db
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2019-03-18 17:03:36 +0100

    factor out settings-component part 1

--- src/components/GridLayout.js
+++ src/components/GridLayout.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import Tile from "./Tile";
 import Ink from "react-ink";
 import { withRouter } from "react-router-dom";
+import SettingsMenu from "./SettingsMenu";
 import TimeseriesTile from "./TimeseriesTile";
 import StatisticsTile from "./StatisticsTile";
 import ExternalTile from "./ExternalTile";
@@ -33,6 +34,7 @@ class GridLayout extends Component {
     };
     this.handleUpdateDimensions = this.handleUpdateDimensions.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.closeSettingsMenu = this.closeSettingsMenu.bind(this);
   }
   componentDidMount() {
     window.addEventListener(
@@ -60,6 +62,12 @@ class GridLayout extends Component {
     });
   }
 
+  closeSettingsMenu() {
+    this.setState({
+      settingsMenu: false
+    });
+  }
+
   toggleMapBackground() {
     const current = this.props.currentMapBackground;
 
@@ -71,6 +79,7 @@ class GridLayout extends Component {
   }
 
   render() {
+    console.log("gridlayout closeSettingsMenu", this.closeSettingsMenu);
     const { width, height, settingsMenu, settingsMenuId } = this.state;
     const { tiles, history } = this.props;
 
@@ -81,165 +90,173 @@ class GridLayout extends Component {
 
     if (settingsMenu) {
       return (
-        <DocumentTitle title="Parramatta | Dashboard | Settings">
-          <div className={styles.SettingsMenu} style={{ height: height }}>
-            <img
-              src={logoCombo}
-              alt="Parramatta dashboard"
-              className={styles.HeaderImage}
-            />
-            <div
-              className={styles.CloseSettings}
-              onClick={() => this.setState({ settingsMenu: false })}
-            >
-              <i className="material-icons">close</i>
-            </div>
-            <div className={styles.SettingsMenuItemsWrapper}>
-              <div
-                className={styles.SettingsMenuItem}
-                onClick={() =>
-                  this.setState({
-                    settingsMenuId: 0
-                  })}
-              >
-                <i className="material-icons">access_time</i>
-                <span
-                  className={`${settingsMenuId === 0
-                    ? styles.ActiveMenu
-                    : null}`}
-                >
-                  Date/Time settings
-                </span>
-              </div>
-
-              <div
-                className={styles.SettingsMenuItem}
-                onClick={() =>
-                  this.setState({
-                    settingsMenuId: 1
-                  })}
-              >
-                <i className="material-icons">layers</i>
-                <span
-                  className={`${settingsMenuId === 1
-                    ? styles.ActiveMenu
-                    : null}`}
-                >
-                  Background layers
-                </span>
-              </div>
-
-              <div
-                className={styles.SettingsMenuItem}
-                onClick={() =>
-                  this.setState({
-                    settingsMenuId: 2
-                  })}
-              >
-                <i className="material-icons">copyright</i>
-                <span
-                  className={`${settingsMenuId === 2
-                    ? styles.ActiveMenu
-                    : null}`}
-                >
-                  Contact
-                </span>
-              </div>
-            </div>
-
-            <main style={{ height: height - 100 }}>
-              {settingsMenuId === 0 ? (
-                <div style={{ padding: 20 }}>
-                  <h4 style={{ padding: 0, margin: 0 }}>
-                    Date/time settings &nbsp;
-                    <button onClick={this.props.resetDateTime}>Reset</button>
-                  </h4>
-                  <hr />
-                  <div className={styles.DateTimePicker}>
-                    <div>
-                      <h5>Date (e.g. "23/12/2018")</h5>
-                      <input
-                        type="date"
-                        name="date"
-                        value={this.props.date}
-                        onChange={event => {
-                          this.props.changeDate(event.target.value);
-                        }}
-                      />
-                    </div>
-                    <div>
-                      <h5>Time (e.g. "09:15 AM")</h5>
-                      <input
-                        type="time"
-                        name="time"
-                        value={this.props.time}
-                        onChange={event => {
-                          this.props.changeTime(event.target.value);
-                        }}
-                      />
-                    </div>
-                  </div>
-                  <br />
-                  <button
-                    className={styles.OKButton}
-                    onClick={() => this.setState({ settingsMenu: false })}
-                  >
-                    OK
-                  </button>
-                </div>
-              ) : null}
-              {settingsMenuId === 1 ? (
-                <div style={{ padding: 20 }}>
-                  <h4 style={{ padding: 0, margin: 0 }}>Map settings</h4>
-                  <hr />
-                  <div className={styles.MapSettings}>
-                    <p>
-                      There are {MAP_BACKGROUNDS
-                        ? MAP_BACKGROUNDS.length
-                        : 0}{" "}
-                      available map background(s):
-                      {MAP_BACKGROUNDS[0].description} and{" "}
-                      {MAP_BACKGROUNDS[1].description}.
-                    </p>
-                    <p>
-                      Currently selected:&nbsp;
-                      <strong>
-                        {this.props.currentMapBackground.description}
-                      </strong>.
-                    </p>
-                    <button onClick={this.toggleMapBackground.bind(this)}>
-                      Switch
-                    </button>
-                  </div>
-                  <br />
-                  <button
-                    className={styles.OKButton}
-                    onClick={() => this.setState({ settingsMenu: false })}
-                  >
-                    OK
-                  </button>
-                </div>
-              ) : null}
-
-              {settingsMenuId === 2 ? (
-                <div style={{ padding: 20 }}>
-                  <h4 style={{ padding: 0, margin: 0 }}>Contact info</h4>
-                  <hr />
-                  <p>
-                    For software issues with the FloodSmart Parramatta System
-                    please contact Nelen & Schuurmans on {nensMail()}. For any
-                    other issues, or suggestions for improvements to the
-                    Parramatta Floodsmart System system, please contact Chris
-                    Gooch on tel.&nbsp;
-                    {chrisTel()} or email {chrisMail()}
-                  </p>
-                </div>
-              ) : null}
-            </main>
-          </div>
-        </DocumentTitle>
+        <SettingsMenu
+          {...this.props}
+          closeSettingsMenu={this.closeSettingsMenu}
+        />
       );
     }
+    // if (settingsMenu) {
+    //   return (
+    //     <DocumentTitle title="Parramatta | Dashboard | Settings">
+    //       <div className={styles.SettingsMenu} style={{ height: height }}>
+    //         <img
+    //           src={logoCombo}
+    //           alt="Parramatta dashboard"
+    //           className={styles.HeaderImage}
+    //         />
+    //         <div
+    //           className={styles.CloseSettings}
+    //           onClick={() => this.setState({ settingsMenu: false })}
+    //         >
+    //           <i className="material-icons">close</i>
+    //         </div>
+    //         <div className={styles.SettingsMenuItemsWrapper}>
+    //           <div
+    //             className={styles.SettingsMenuItem}
+    //             onClick={() =>
+    //               this.setState({
+    //                 settingsMenuId: 0
+    //               })}
+    //           >
+    //             <i className="material-icons">access_time</i>
+    //             <span
+    //               className={`${settingsMenuId === 0
+    //                 ? styles.ActiveMenu
+    //                 : null}`}
+    //             >
+    //               Date/Time settings
+    //             </span>
+    //           </div>
+
+    //           <div
+    //             className={styles.SettingsMenuItem}
+    //             onClick={() =>
+    //               this.setState({
+    //                 settingsMenuId: 1
+    //               })}
+    //           >
+    //             <i className="material-icons">layers</i>
+    //             <span
+    //               className={`${settingsMenuId === 1
+    //                 ? styles.ActiveMenu
+    //                 : null}`}
+    //             >
+    //               Background layers
+    //             </span>
+    //           </div>
+
+    //           <div
+    //             className={styles.SettingsMenuItem}
+    //             onClick={() =>
+    //               this.setState({
+    //                 settingsMenuId: 2
+    //               })}
+    //           >
+    //             <i className="material-icons">copyright</i>
+    //             <span
+    //               className={`${settingsMenuId === 2
+    //                 ? styles.ActiveMenu
+    //                 : null}`}
+    //             >
+    //               Contact
+    //             </span>
+    //           </div>
+    //         </div>
+
+    //         <main style={{ height: height - 100 }}>
+    //           {settingsMenuId === 0 ? (
+    //             <div style={{ padding: 20 }}>
+    //               <h4 style={{ padding: 0, margin: 0 }}>
+    //                 Date/time settings &nbsp;
+    //                 <button onClick={this.props.resetDateTime}>Reset</button>
+    //               </h4>
+    //               <hr />
+    //               <div className={styles.DateTimePicker}>
+    //                 <div>
+    //                   <h5>Date (e.g. "23/12/2018")</h5>
+    //                   <input
+    //                     type="date"
+    //                     name="date"
+    //                     value={this.props.date}
+    //                     onChange={event => {
+    //                       this.props.changeDate(event.target.value);
+    //                     }}
+    //                   />
+    //                 </div>
+    //                 <div>
+    //                   <h5>Time (e.g. "09:15 AM")</h5>
+    //                   <input
+    //                     type="time"
+    //                     name="time"
+    //                     value={this.props.time}
+    //                     onChange={event => {
+    //                       this.props.changeTime(event.target.value);
+    //                     }}
+    //                   />
+    //                 </div>
+    //               </div>
+    //               <br />
+    //               <button
+    //                 className={styles.OKButton}
+    //                 onClick={() => this.setState({ settingsMenu: false })}
+    //               >
+    //                 OK
+    //               </button>
+    //             </div>
+    //           ) : null}
+    //           {settingsMenuId === 1 ? (
+    //             <div style={{ padding: 20 }}>
+    //               <h4 style={{ padding: 0, margin: 0 }}>Map settings</h4>
+    //               <hr />
+    //               <div className={styles.MapSettings}>
+    //                 <p>
+    //                   There are {MAP_BACKGROUNDS
+    //                     ? MAP_BACKGROUNDS.length
+    //                     : 0}{" "}
+    //                   available map background(s):
+    //                   {MAP_BACKGROUNDS[0].description} and{" "}
+    //                   {MAP_BACKGROUNDS[1].description}.
+    //                 </p>
+    //                 <p>
+    //                   Currently selected:&nbsp;
+    //                   <strong>
+    //                     {this.props.currentMapBackground.description}
+    //                   </strong>.
+    //                 </p>
+    //                 <button onClick={this.toggleMapBackground.bind(this)}>
+    //                   Switch
+    //                 </button>
+    //               </div>
+    //               <br />
+    //               <button
+    //                 className={styles.OKButton}
+    //                 onClick={() => this.setState({ settingsMenu: false })}
+    //               >
+    //                 OK
+    //               </button>
+    //             </div>
+    //           ) : null}
+
+    //           {settingsMenuId === 2 ? (
+    //             <div style={{ padding: 20 }}>
+    //               <h4 style={{ padding: 0, margin: 0 }}>Contact info</h4>
+    //               <hr />
+    //               <p>
+    //                 For software issues with the FloodSmart Parramatta System
+    //                 please contact Nelen & Schuurmans on {nensMail()}. For any
+    //                 other issues, or suggestions for improvements to the
+    //                 Parramatta Floodsmart System system, please contact Chris
+    //                 Gooch on tel.&nbsp;
+    //                 {chrisTel()} or email {chrisMail()}
+    //               </p>
+    //             </div>
+    //           ) : null}
+    //         </main>
+    //       </div>
+    //     </DocumentTitle>
+    //   );
+    // }
 
     const tileComponents = tiles.map(tile => {
       const shortTitle = tile.shortTitle || tile.title;
