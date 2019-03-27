6baf4f91688d33b05812d3d217943c7c7ca47143
Tom de Boer <Derryrover@users.noreply.github.com>
2019-03-27 09:13:33 +0100

    remove fix save zoom in state (#138)
    * remove fix save zoom in state
    
    * use axes instead of wantedaxes

--- src/components/TimeseriesChart.js
+++ src/components/TimeseriesChart.js
@@ -32,12 +32,9 @@ class TimeseriesChartComponent extends Component {
       ...curPer,
       componentHasMountedOnce: false,
       componentRef: "comp-" + parseInt(Math.random(), 10),
-      wantedAxes: null,
       combinedEvents: null,
       isFinishedFetchingRasterEvents: false,
-      isFinishedFetchingTimeseriesEvents: false,
-      // keep xaxis of plotly graph in case data is refreshed by timer but user zoomed in and likes to preserve the zoom level
-      zoomedXaxis: null
+      isFinishedFetchingTimeseriesEvents: false
     };
 
     this._areAllRasterEventsLoaded = this._areAllRasterEventsLoaded.bind(this);
@@ -98,8 +95,7 @@ class TimeseriesChartComponent extends Component {
     );
 
     this.setState({
-      combinedEvents,
-      wantedAxes: axes
+      combinedEvents
     });
   }
 
@@ -711,13 +707,6 @@ class TimeseriesChartComponent extends Component {
     const verticalOffset =
       Math.round(this.props.height / 2) - Math.round(SPINNER_SIZE / 2);
 
-    // set Xaxis of state in case user is zoomed in so zooming is preserved if component is updated by time refresh
-    let tmpLayout = this.getLayout(this.state.wantedAxes, thresholds);
-    // only set xaxis if it is actually set in state of component
-    if (this.state.zoomedXaxis) {
-      tmpLayout.xaxis = this.state.zoomedXaxis;
-    }
-
     return (
       <div
         id={this.state.componentRef}
@@ -734,15 +723,8 @@ class TimeseriesChartComponent extends Component {
           <Plot
             className="fullPlot"
             data={combinedEvents}
-            layout={tmpLayout}
+            layout={this.getLayout(axes, thresholds)}
             config={{ displayModeBar: true }}
-            onUpdate={figure => {
-              // set Xaxis of state when user zoomes in so zooming is preserved if component is updated by time refresh
-              this.setState({ zoomedXaxis: figure.layout.xaxis });
-            }}
-            onDoubleClick={e => {
-              this.setState({ zoomedXaxis: null });
-            }}
           />
         ) : (
           <div
@@ -784,7 +766,7 @@ class TimeseriesChartComponent extends Component {
           <Plot
             className="gridPlot"
             data={combinedEvents}
-            layout={this.getLayout(this.state.wantedAxes)}
+            layout={this.getLayout(axes)}
             config={{ displayModeBar: false }}
           />
         ) : (
