283503876671ad956517d002fb07cb77c2d7c4cf
Tom de Boer <tom.deboer@nelen-schuurmans.nl>
2019-06-07 15:01:27 +0200

    use newest code for parsing url

--- src/TermsOrLoginOrApp.js
+++ src/TermsOrLoginOrApp.js
@@ -63,13 +63,11 @@ class TermsOrLoginOrAppComponent extends Component {
 
     // split on /floodsmart/
     // slashes are included in split so we do not also split on the second floodsmart in /floodsmart/floodsmart
-    // const urlPreQueryParameters = window.location.href.split('?')[0];
-    // const urlPostDashboard = urlPreQueryParameters.split("/floodsmart/")[1];
-    const urlPostDashboard = window.location.href.split("/floodsmart/")[1];
+    const urlPreQueryParameters = window.location.href.split("?")[0];
+    const urlPostDashboard = urlPreQueryParameters.split("/floodsmart/")[1];
 
     if (
-      !urlPostDashboard || // if there was no /floodsmart/ in the url, should only happen in dev or with a url of /floodsmart (no tailing slash)
-      urlPostDashboard[0] === "?" // or if the rest of the url starts with a question mark (?), this happens when the url has ?iframe=true as parameter
+      !urlPostDashboard // if there was no /floodsmart/ in the url, should only happen in dev or with a url of /floodsmart (no tailing slash)
     ) {
       return undefined;
     }
